275012866d81e01f022791b570861fa0c988b269
Leon Knauer <git@leonknauer.com>
2021-02-01 19:08:09 +0100

    JS compilen

--- schoolnn/webpack.mix.js
+++ schoolnn/webpack.mix.js
@@ -5,8 +5,8 @@ mix.setPublicPath("public")
     .postCss("resources/css/tailwind.css", "public/css/app.css", [
         require("tailwindcss"),
         require('postcss-nested'),
-    ]);
-    //.js('resources/js/app.js', 'public/js/app.js').vue();
+    ])
+    .js('resources/js/app.js', 'public/js/app.js').vue();
 
 
 if (mix.inProduction()) {
