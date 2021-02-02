d69b2be5a7ff9ed5efd20a47f04372f9d0e1c20f
mawalu <web@mawalabs.de>
2021-02-02 14:15:19 +0100

    Config and docs

--- schoolnn_app/settings.py
+++ schoolnn_app/settings.py
@@ -20,15 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-def _get_password() -> str:
-    return ""
-
-
-SECRET_KEY = _get_password()  # TO DO: read from config
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS: List[str] = []
 
