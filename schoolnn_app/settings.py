d34af43fe2ee50846eda0c627db30397a86cecc8
Leon Knauer <git@leonknauer.com>
2021-02-03 11:27:44 +0100

    Template-Ordner spezifizieren

--- schoolnn_app/settings.py
+++ schoolnn_app/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "schoolnn_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "schoolnn/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
