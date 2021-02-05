15945c917c2e9a26f4983fb89b6efa0cf608071f
mawalu <web@mawalabs.de>
2021-02-05 17:31:26 +0100

    Fix checkstyle

--- schoolnn/urls.py
+++ schoolnn/urls.py
@@ -1,14 +1,18 @@
-# flake8: noqa
+""" Django routes """
 
 from django.urls import path
-from schoolnn.views.datasets import DatasetCreate, DatasetList, DatasetDetail, DatasetUpdate, DatasetDelete
+from schoolnn.views.datasets import DatasetCreate, DatasetList
+from schoolnn.views.datasets import DatasetDetail, DatasetUpdate, DatasetDelete
 from schoolnn.views.images import ImageView
 
 urlpatterns = [
     path("datasets/", DatasetList.as_view(), name="dataset-list"),
-    path("datasets/<int:pk>/", DatasetDetail.as_view(), name="dataset-detail"),
+    path("datasets/<int:pk>/", DatasetDetail.as_view(),
+         name="dataset-detail"),
     path("datasets/add/", DatasetCreate.as_view(), name="dataset-add"),
-    path("datasets/<int:pk>/edit/", DatasetUpdate.as_view(), name="dataset-edit"),
-    path("datasets/<int:pk>/delete/", DatasetDelete.as_view(), name="dataset-delete"),
+    path("datasets/<int:pk>/edit/", DatasetUpdate.as_view(),
+         name="dataset-edit"),
+    path("datasets/<int:pk>/delete/", DatasetDelete.as_view(),
+         name="dataset-delete"),
     path("images/<int:pk>", ImageView.as_view(), name="image-show")
 ]
--- schoolnn/views/datasets.py
+++ schoolnn/views/datasets.py
@@ -1,8 +1,8 @@
-# flake8: noqa
-
 import os
 import shutil
 import zipfile
+from typing import Optional
+
 from django import forms
 from django.core.validators import FileExtensionValidator
 from django.http import HttpResponseRedirect
@@ -38,11 +38,18 @@ class DatasetCreate(CreateView):
     form_class = DatasetCreateForm
     template_name = "datasets/form.html"
 
+    object: Optional[Dataset] = None
+    team_dir: Optional[str] = None
+    upload_file: Optional[str] = None
+    extract_dir: Optional[str] = None
+
     def form_valid(self, form: DatasetCreateForm):
         self.object = form.save()
         self.team_dir = "storage/1/"
-        self.upload_file = "{}/upload_{}.zip".format(self.team_dir, self.object.id)
-        self.extract_dir = "{}/{}_upload/".format(self.team_dir, self.object.id)
+        self.upload_file = "{}/upload_{}.zip".format(self.team_dir,
+                                                     self.object.id)
+        self.extract_dir = "{}/{}_upload/".format(self.team_dir,
+                                                  self.object.id)
 
         self.handle_upload(self.request.FILES["file"])
         self.create_tags(self.object)
@@ -63,20 +70,23 @@ class DatasetCreate(CreateView):
             zip_ref.extractall(self.extract_dir)
 
     def create_tags(self, dataset: Dataset):
-        os.makedirs(os.path.join(self.team_dir, str(dataset.id)), exist_ok=True)
+        os.makedirs(os.path.join(str(self.team_dir), str(dataset.id)),
+                    exist_ok=True)
 
         for entry in os.scandir(self.extract_dir):
             if entry.is_dir():
-                label = Label.objects.create(name=entry.name, dataset=dataset)
+                label = Label.objects.create(name=entry.name,
+                                             dataset=dataset)
                 self.process_images(entry, label, dataset)
 
-    def process_images(self, path: os.DirEntry, label: Label, dataset: Dataset):
+    def process_images(self, path: os.DirEntry, label: Label,
+                       dataset: Dataset):
         for entry in os.scandir(path):
             image = Image.objects.create(dataset=dataset)
             ImageLabel.objects.create(label=label, image=image)
             im = PIL_Image.open(entry.path)
             im = ImageOps.fit(im, (512, 512), PIL_Image.ANTIALIAS)
-            im.save(os.path.join(self.team_dir, image.path))
+            im.save(os.path.join(str(self.team_dir), image.path))
 
 
 class DatasetUpdate(UpdateView):
