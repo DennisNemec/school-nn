3ebf8291d72701b07c0a1f01da6bb4c2f90ad0da
Leon Knauer <git@leonknauer.com>
2021-02-05 16:25:11 +0100

    Assets, Font und Farben aus Figma extrahiert

--- schoolnn/tailwind.config.js
+++ schoolnn/tailwind.config.js
@@ -4,14 +4,35 @@ module.exports = {
         purgeLayersByDefault: true,
     },
     purge: {
-        enabled: true, //true for production build
+        enabled: false, //true for production build
         content: [
             'templates/*.html',
             'templates/**/*.html'
         ]
     },
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary": "#334D6E",
+                "light-gray": "#F1F3F5",
+                "text-gray": "#90A0B7",
+                "black": "#192A3E",
+                "selected": "#109CF1",
+                "green": "#2ED47A"
+            },
+
+            spacing: {
+                "screen-minus-header": "calc(100vh - 6rem)",
+            },
+
+            boxShadow: {
+                "base": "6px 6px 18px rgba(0, 0, 0, 0.06)",
+            },
+
+            fontFamily: {
+                "sans": ['"Poppins"', 'sans-serif'],
+            }
+        },
     },
     variants: {},
     plugins: [],
