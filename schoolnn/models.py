f342c1b07e10f9e368a67aee1d01c4274b8a86ef
Julian Büttner <buettner-julian@-online.de>
2021-02-05 11:48:24 +0100

    Edited models to make pipeline go green

--- schoolnn/models.py
+++ schoolnn/models.py
@@ -1,10 +1,12 @@
-"""Manages TODO."""
+"""All ORM models."""
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
 class Workspace(models.Model):
+    """The equivalent of a class room."""
+
     name = models.CharField(max_length=30)
     settings_json = models.JSONField()
     created_at = models.DateTimeField()
@@ -12,7 +14,9 @@ class Workspace(models.Model):
 
 
 class User(models.Model):
-    password =models.CharField(max_length=50)
+    """User account of students, teachers and admins."""
+
+    password = models.CharField(max_length=50)
     last_login = models.DateTimeField()
     is_active = models.BooleanField(default=False)
     username = models.CharField(max_length=15)
@@ -26,6 +30,8 @@ class User(models.Model):
 
 
 class Dataset(models.Model):
+    """Set of images used for training."""
+
     name = models.CharField(max_length=15)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     custom = models.BooleanField(default=False)
@@ -34,19 +40,29 @@ class Dataset(models.Model):
 
 
 class Image(models.Model):
+    """Image of a dataset used for training."""
+
     dataset_id = models.ForeignKey(Dataset, on_delete=models.CASCADE)
     filename = models.FilePathField()
 
 
 class Label(models.Model):
+    """Label existing in a database."""
+
     dataset_id = models.ForeignKey(Dataset, on_delete=models.CASCADE)
     name = models.CharField(max_length=15)
 
-class Image_Label(models.Model):
+
+class ImageLabel(models.Model):
+    """Link between one image and one label."""
+
     label_id = models.ForeignKey(Label, on_delete=models.CASCADE)
     image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
 
+
 class Architecture(models.Model):
+    """One sequential neural network architecure."""
+
     name = models.CharField(max_length=15)
     custom = models.BooleanField(default=False)
     architecture_json = models.JSONField
@@ -55,17 +71,21 @@ class Architecture(models.Model):
 
 
 class Project(models.Model):
+    """One project a user/student works on."""
+
     name = models.CharField(max_length=15)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     custom = models.BooleanField(default=False)
     dataset_id = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    architecture_id = models.ForeignKey(Architecture, on_delete=models.CASCADE)
-    traning_pass_id = models.ForeignKey(Training_Pass, on_delete=models.CASCADE)
+    architecture_id = models.ForeignKey(
+        Architecture, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
 
-class Training_Pass(models.Model):
+class TrainingPass(models.Model):
+    """One training pass of a project."""
+
     name = models.CharField(max_length=15)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
@@ -76,11 +96,18 @@ class Training_Pass(models.Model):
     model_weight = models.BinaryField()
     status = models.CharField(max_length=15)
 
-class Training_Step_Metrics(models.Model):
-    traning_pass_id = models.ForeignKey(Training_Pass, on_delete=models.CASCADE)
+
+class TrainingStepMetrics(models.Model):
+    """Training and validation metrics of a training block/step."""
+
+    traning_pass_id = models.ForeignKey(
+        TrainingPass, on_delete=models.CASCADE)
     metrics_json = models.JSONField()
 
+
 class Note(models.Model):
+    """Note of an object."""
+
     text = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
@@ -88,12 +115,13 @@ class Note(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+
 class Visiblity(models.Model):
+    """Defines who can see or edit an object."""
+
     permissions = models.JSONField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     object_type = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-
-
