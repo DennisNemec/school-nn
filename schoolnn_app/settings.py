fe4eed78571f3d72c1dc8aabc66680046888eb94
Leon Knauer <git@leonknauer.com>
2021-02-05 16:25:41 +0100

    Static URL anpassen

--- schoolnn_app/settings.py
+++ schoolnn_app/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/assets/"
+STATIC_URL = "/public/"
 STATICFILES_DIRS = [
     "./schoolnn/public",
 ]
