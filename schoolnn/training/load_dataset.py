9853c5a17235b61ee51bdb94e3531e85093a75d4
Julian Büttner <buettner-julian@-online.de>
2021-03-01 18:12:28 +0100

    Fix: readded missing shuffle

--- schoolnn/training/load_dataset.py
+++ schoolnn/training/load_dataset.py
@@ -1,6 +1,7 @@
 """Generate training and validation image lists."""
 from typing import List, Tuple
 from ..models import TrainingPass, Image
+from random import seed, shuffle
 
 
 def get_training_and_validation_images(
@@ -16,6 +17,9 @@ def get_training_and_validation_images(
     validation_split: float = training_pass.training_parameter.validation_split
 
     validation_items_count = max(1, int(len(image_list) * validation_split))
+
+    seed(training_pass.id)
+    shuffle(image_list)
 
     training_list = image_list[:-validation_items_count]
     validation_list = image_list[-validation_items_count:]
