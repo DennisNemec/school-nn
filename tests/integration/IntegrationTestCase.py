d272a3bedff7c2c48e953d3bc6c3cbb2554aadce
mawalu <web@mawalabs.de>
2021-03-11 17:39:53 +0100

    Add browser based tests using pyppeteer

---
+++ tests/integration/IntegrationTestCase.py
@@ -0,0 +1,43 @@
+import asyncio
+from typing import Coroutine, Any
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from pyppeteer import launch
+from pyppeteer.browser import Browser
+from pyppeteer.page import Page
+
+from schoolnn.models import User
+
+
+class IntegrationTestCase(StaticLiveServerTestCase):
+    browser: Browser
+    page: Page
+    user: User
+    password: str
+
+    def setUp(self) -> None:
+        self.password = 'super_secure'
+        self.user = User.objects.create(username='peter', password=make_password(self.password))
+
+    @staticmethod
+    def get_browser() -> Coroutine[Any, Any, Browser]:
+        return launch(handleSIGINT=False, handleSIGTERM=False, handleSIGHUP=False, headless=False)
+
+    async def asyncSetUp(self):
+        self.browser = await self.get_browser()
+        self.page = await self.browser.newPage()
+
+    async def asyncTearDown(self):
+        await self.browser.close()
+
+    async def login(self, username, password):
+        await self.page.goto("{}{}".format(self.live_server_url, '/login/'))
+
+        await self.page.type('#id_username', username)
+        await self.page.type('#id_password', password)
+
+        await asyncio.wait([
+            self.page.click('[type=submit]'),
+            self.page.waitForNavigation()
+        ])
