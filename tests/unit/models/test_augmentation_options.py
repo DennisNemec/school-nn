5f2e6076ed8e4fe6a94ef7ecd8307f1c1dfeb923
Julian Büttner <buettner-julian@-online.de>
2021-03-19 14:19:01 +0100

    Add more augmentation tests

--- tests/unit/models/test_augmentation_options.py
+++ tests/unit/models/test_augmentation_options.py
@@ -3,33 +3,69 @@ from schoolnn.models import AugmentationOptions
 from numpy import array_equal, random
 
 
-def test_augmentation_options_from_to_dict():
-    """Create some object, dump and read them back."""
-    keys = [
-        "channel_shuffle",
-        "brightness",
-        "gaussian_noise",
-        "dropout_boxes",
-        "salt_and_pepper",
-        "jpeg_artifacts",
-        "vertical_flip",
-        "distortion",
-        "rotate",
-        "scale_and_translate",
-        "color",
-    ]
+_KEYS = [
+    "channel_shuffle",
+    "brightness",
+    "gaussian_noise",
+    "dropout_boxes",
+    "salt_and_pepper",
+    "jpeg_artifacts",
+    "vertical_flip",
+    "distortion",
+    "rotate",
+    "scale_and_translate",
+    "color",
+]
 
-    for disabled_option in keys:
+
+def test_augmentation_options_from_to_dict():
+    """Create some options, dump and read them back."""
+    for disabled_option in _KEYS:
         # Activate everything but one option
         kwargs = {}
-        for key in keys:
+        for key in _KEYS:
             kwargs[key] = True
         kwargs[disabled_option] = False
 
-        obj = AugmentationOptions(**kwargs)
+        opt = AugmentationOptions(**kwargs)
+        assert opt.get_augmenter()  # Don't fail
 
-        assert obj.to_dict() == kwargs
-        assert obj.activated_count() == len(keys) - 1
+        assert opt.to_dict() == kwargs
+        assert opt.activated_count() == len(_KEYS) - 1
+
+
+def test_augmentation_options_from_to_dict2():
+    """Create some options, dump and read them back."""
+    for enabled_option in _KEYS:
+        # Activate nothing but one option
+        kwargs = {}
+        for key in _KEYS:
+            kwargs[key] = False
+        kwargs[enabled_option] = True
+
+        opt = AugmentationOptions(**kwargs)
+        assert opt.get_augmenter()  # Don't fail
+
+        assert opt.to_dict() == kwargs
+        assert opt.activated_count() == 1
+
+
+def test_augmentation_options_identity():
+    """Check if identity applies when no option is enabled."""
+    kwargs = {}
+    for key in _KEYS:
+        kwargs[key] = False
+
+    opt = AugmentationOptions(**kwargs)
+    assert opt.to_dict() == kwargs
+    assert opt.activated_count() == 0
+
+    random_image_array = 255 * random.rand(28, 28, 3)
+    random_image_array = random_image_array.astype("uint8")
+
+    augmented_image = opt.get_augmenter()(image=random_image_array)
+
+    assert array_equal(augmented_image, random_image_array)
 
 
 def test_augmentation_options_augmentation():
